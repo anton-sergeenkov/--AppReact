25915b69c9f2d9fa9ca8e27f0af0db0f45f86287
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-04-26 18:18:56 +0300

    start

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './App.css';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<img src={logo} />
+			</div>
+		);
+	}
+}
