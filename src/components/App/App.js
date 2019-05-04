233968889821012b66b574d594e088c04e75ec37
anton-sergeenkov <warnes23@yandex.ru>
2019-05-04 18:46:25 +0300

    1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ComponentTest from '../../containers/ComponentTest';
+import logo from '../../assets/logo.svg';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<h1>Привет</h1>
+				<div class="img"></div>
 				<ComponentTest />
+				<img src={logo} width="200px" />
 			</div>
 		);
 	}
