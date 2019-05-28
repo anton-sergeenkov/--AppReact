12faa62b588f9e60c606b9b2f512c01f1e57c58f
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-05-28 14:06:47 +0300

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,17 +2,15 @@ import axios from 'axios';
 import { REQUEST_SERVER } from '../constants';
 import { GET_CATALOG, SET_CATALOG_SEARCH } from './actionTypes';
 
-export const getCatalog = function() {
-    return function(dispatch) {
-        axios.get(REQUEST_SERVER.URL)
-		.then(response => {
-            dispatch({
-                type: GET_CATALOG,
-                payload: response.data,
-            })
-		})
-        .catch(error => console.log(error));
-    }
+export const getCatalog = () => (dispatch) => {
+    axios.get(REQUEST_SERVER.URL)
+    .then(response => {
+        dispatch({
+            type: GET_CATALOG,
+            payload: response.data,
+        })
+    })
+    .catch(error => console.log(error));
 }
 
 export const setCatalogSearch = searchValue => ({
